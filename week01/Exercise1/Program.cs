f6db11718dd82e8ddd8fbc784f97315efefddd98
Bryan Núñez <bryanunez.ai@gmail.com>
2025-11-07 22:34:40 -0500

    Completed W1 Excersices 1 and 2

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        /// Get first name
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        /// Get last name
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        /// Create full name
+        string fullName = firstName + " " + lastName;
+
+        /// Print name as cool as James Bond would do it
+        Console.WriteLine($"Your name is {lastName}, {fullName}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,89 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        /// Ask grade percetnage
+        Console.WriteLine("Please enter your grade percentage:");
+        int gradePercentage = int.Parse(Console.ReadLine());
+        string gradeLetter = "N/A";
+
+        /// Validate input
+        if (gradePercentage < 0 || gradePercentage > 100)
+        {
+            Console.WriteLine("Invalid grade percentage. Must be between 0 and 100.");
+        }
+        else
+        {
+            /// Assign letter grade
+            if (gradePercentage >= 90)
+            {
+                gradeLetter = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                gradeLetter = "B";
+            }
+            else if (gradePercentage >= 70)
+            {
+                gradeLetter = "C";
+            }
+            else if (gradePercentage >= 60)
+            {
+                gradeLetter = "D";
+            }
+            else
+            {
+                gradeLetter = "F";
+            }
+
+            /// Stretch Challenge
+            /// Assign + or - to letter grade
+
+            /// Get remainder
+            int remainder = gradePercentage % 10;
+            string sign = "";
+
+            /// Assign sign
+            if (remainder >= 7)
+            {   
+                /// Handle Exceptions
+                if (gradeLetter == "A" || gradeLetter == "F")
+                {
+                    sign = "";
+                }
+                else
+                {
+                    sign = "+";
+                }
+            }
+            else if (remainder < 3)
+            {   
+                // Handle Exceptions
+                if (gradeLetter == "F")
+                {
+                    sign = "";
+                }
+                else
+                {
+                    sign = "-";
+                }
+            }
+            else
+            {
+                sign = "";
+            }
+
+            /// Print letter grade if valid percentage was entered.
+            Console.WriteLine($"Your grade is {gradeLetter}{sign}.");
+            /// Congratulate or print sad message :(
+            if (gradeLetter == "A" || gradeLetter == "B" || gradeLetter == "C")
+            {
+                Console.WriteLine("Congratulations! You passed!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you failed. :(, try again next time. You can do it!");
+            }  
+        }
     }
 }
