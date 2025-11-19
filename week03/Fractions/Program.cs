35256d530316045b4a768bccb7efe963a3d0ec8c
Bryan Núñez <bryanunez.ai@gmail.com>
2025-11-19 00:05:30 -0500

    Completed Encapsulation Learning Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        // Instance for 1/1
+        Fraction fraction_1 = new Fraction();
+        Console.WriteLine(fraction_1.GetFractionString());
+        Console.WriteLine(fraction_1.GetDecimalValue());
+
+        // Instance for 5
+        Fraction fraction_2 = new Fraction(5);
+        Console.WriteLine(fraction_2.GetFractionString());
+        Console.WriteLine(fraction_2.GetDecimalValue());
+
+        // Instance for 3/4                     
+        Fraction fraction_3 = new Fraction(3, 4);
+        Console.WriteLine(fraction_3.GetFractionString());
+        Console.WriteLine(fraction_3.GetDecimalValue());
+
+        // Instance for 1/3                     
+        Fraction fraction_4 = new Fraction(1, 3);
+        Console.WriteLine(fraction_4.GetFractionString());
+        Console.WriteLine(fraction_4.GetDecimalValue());
+
     }
 }
